af27ef5139dd5aa07b820aaaf00b6a9c967f3534
ramgos <ramka2006@gmail.com>
2020-10-18 18:54:08 +0300

    bee

--- bot.py
+++ bot.py
@@ -1,2 +1,34 @@
 import discord
+import secrets
+import math
+import re
 
+client = discord.Client()
+
+bidentifier = re.compile(r"\b(bee)\b", re.I)
+file = open("beemovie.txt", "r")
+beemovie = file.read()
+
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if bidentifier.match(message.content.lower()):
+        rem = len(beemovie) % 2000
+        reps = len(beemovie) / 2000
+        reps = math.floor(reps)
+
+        chunk = beemovie[reps*2000:]
+
+        for i in range(reps):
+            start = i*2000
+            end = i + 1
+            end = end * 2000
+            end = end - 1
+            await message.channel.send(beemovie[start:end])
+
+        await message.channel.send(chunk)
+
+client.run(secrets.get_key())
